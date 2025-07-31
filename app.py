9d1fc00d6377956feeb82cd8d050c15f2b4586bc
khilarepriya <p.khilare@accenture.com>
2025-07-31 11:35:52 +0530

    Update Jenkinsfile

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-from flask import Flask
+from flask import Flask, Response
+
 app = Flask(__name__)
+
+@app.after_request
+def set_security_headers(response: Response):
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Content-Security-Policy'] = "default-src 'self'"
+    response.headers['Permissions-Policy'] = "geolocation=(), camera=()"
+    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+    response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
+    return response
 
 @app.route("/")
 def hello():
@@ -7,3 +18,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5020)
+
